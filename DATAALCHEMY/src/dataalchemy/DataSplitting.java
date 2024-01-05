baf271ed26a88fe0db4d6fb6de078d770489fce3
shaurahsasha <102988593+shaurahsasha@users.noreply.github.com>
2024-01-05 23:25:59 +0800

    new and updated data splitting

--- DATAALCHEMY/src/dataalchemy/DataSplitting.java
+++ DATAALCHEMY/src/dataalchemy/DataSplitting.java
@@ -10,4 +10,127 @@ package dataalchemy;
  */
 public class DataSplitting {
     
+    private List<String[]> transformedArray;
+    private List<String[]> shuffledArray;
+    private List<String[]> trainingSet;
+    private List<String[]> testingSet;
+
+    public DataSplitting(List<String[]> transformedArray) {
+        this.transformedArray = transformedArray;
+        shuffle();
+        split();
+    }
+    
+    private void shuffle() {
+        
+        DataLoading a = new DataLoading("transformedhousepricing.csv", false);
+        List<String[]> arrayList = a.getLoadedDataset();
+        
+        System.out.println("-- Shuffling the dataset ...");
+        String cleanedFilePath = "shuffledhousepricing.csv";
+
+        try {
+            PrintWriter out = new PrintWriter(new FileOutputStream(cleanedFilePath));
+            
+            int n = arrayList.size();
+            Random random = new Random();
+            for (int i = 0; i < n; i++) {
+
+                if(i == 0) {
+                    continue;
+                }
+
+                int change = random.nextInt(n - i) + i;
+                String[] temp = arrayList.get(i);
+                arrayList.set(i, arrayList.get(change)) ;
+                arrayList.set(change, temp);
+            }            
+            
+            for(String[] rows: arrayList) {      
+                StringBuilder rowBuilder = new StringBuilder();
+
+                for(String columns: rows) {  
+                    rowBuilder.append(columns).append(",");
+                }
+                
+                if (rowBuilder.length() > 0) {
+                    // Remove the last comma before writing the row
+                    out.println(rowBuilder.substring(0, rowBuilder.length() - 1));
+                }
+            }
+            out.close();
+            System.out.println("-> Successfully saved shuffled data to: " +cleanedFilePath + "!\n");
+            
+        } catch (IOException e) {
+            System.out.println("Problem with file output\n");
+        }
+    }
+    
+    private void split() {
+        
+        DataLoading a = new DataLoading("shuffledhousepricing.csv", false);
+        List<String[]> arrayList = a.getLoadedDataset();
+        
+        System.out.println("-- Splitting the dataset ...");
+        String trainingSetFilePath = "traininghousepricing.csv";
+        String testingSetFilePath = "testinghousepricing.csv";
+
+
+        try {
+            PrintWriter trainingOutputStream = new PrintWriter(new FileOutputStream(trainingSetFilePath));
+            PrintWriter testingOutputStream = new PrintWriter(new FileOutputStream(testingSetFilePath));       
+            
+            int rowCounter = 0;
+            for(String[] rows: arrayList) {      
+                StringBuilder rowBuilder = new StringBuilder();
+                
+                if(rowCounter == 0) {
+                    
+                    for(String columns: rows) {  
+                            rowBuilder.append(columns).append(",");
+                    }
+                    if (rowBuilder.length() > 0) {
+                        // Remove the last comma before writing the row
+                        trainingOutputStream.println(rowBuilder.substring(0, rowBuilder.length() - 1));
+                        testingOutputStream.println(rowBuilder.substring(0, rowBuilder.length() - 1));
+                    }
+                } else if (rowCounter <= (int)arrayList.size()*0.7) {
+                    
+                    for(String columns: rows) {  
+                            rowBuilder.append(columns).append(",");
+                    }
+                    if (rowBuilder.length() > 0) {
+                        // Remove the last comma before writing the row
+                        trainingOutputStream.println(rowBuilder.substring(0, rowBuilder.length() - 1));
+                    }
+                    
+                } else {
+                    for(String columns: rows) {  
+                        rowBuilder.append(columns).append(",");
+                    }
+
+                    if (rowBuilder.length() > 0) {
+                        // Remove the last comma before writing the row
+                        testingOutputStream.println(rowBuilder.substring(0, rowBuilder.length() - 1));
+                    }
+                }
+                
+                rowCounter++;
+            }
+            trainingOutputStream.close();
+            testingOutputStream.close();
+            System.out.println("-> Successfully saved the training set to: " + trainingSetFilePath + "!");
+            System.out.println("-> Successfully saved the testing set to: " + testingSetFilePath + "!\n");
+
+        } catch (IOException e) {
+            System.out.println("Problem with file output\n");
+        }
+    }
+    
+    public void getShuffledDataset(int lines) {
+        DataLoading a = new DataLoading("shuffledhousepricing.csv");
+        System.out.println("Shuffled House Pricing Dataset: ");
+        a.getDataset(lines);
+    }
+
 }
